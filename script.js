3fbeb87603c47c105d63d265e211dbd68e0347bc
Luisgrande717 <luisgrande717@outlook.com>
2025-05-05 15:27:17 -0400

    finished lab

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let csv = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26'
+//let csv = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26'
+
+let csv = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26\n7,Bilbo,None,111';
+
+
 
 let cell = ''
 let table = []
@@ -41,6 +45,8 @@ console.log(table)
 
 // console.log(data);
 
+//******************************************this code is more dynamic using .map and .split************ */
+// *****************************************Alot of googlegling so I dont want to use it but it works******
 // ---------------------------Part 3 -----------------------------------------------------------
 let table_obj = []
 let row_template = {
@@ -60,6 +66,36 @@ for (let r=1; r < table.length; r++) {
 console.log(table_obj)
 
 //------------------------------Part 4 --------------------------------------------------------------
-table_obj = table_obj.pop;
+// Remove the last element
+table_obj.pop();
+
+// Insert new object at index 1
+let newEntry = { id: "48", name: "Barry", occupation: "Runner", age: "25" };
+table_obj.splice(1, 0, newEntry); // Inserts at index 1
+
 console.log(table_obj);
+
+let totalAge = 0;
+for (let i = 0; i < table_obj.length; i++) {
+    totalAge += parseInt(table_obj[i].age, 10); // Convert age to an integer and add to total
+}
+
+let averageAge = totalAge / table_obj.length; // Divide by total number of objects
+console.log(`Average Age: ${averageAge}`);
+
+
+
+
+// ------------------------------Part 5 ---------------------------------------------------------------
+let csvString = 'ID,Name,Occupation,Age\n';
+
+csvString += '42,Bruce,Knight,41\n';
+csvString += '48,Barry,Runner,25\n';
+csvString += '57,Bob,Fry Cook,19\n';
+csvString += '63,Blaine,Quiz Master,58\n';
+csvString += '07,Bilbo,None,111\n';
+
+console.log(csvString);
+
+
 
