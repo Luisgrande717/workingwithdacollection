cabfa0e03844d54e64a649bf96b09922e20fa059
Luisgrande717 <luisgrande717@outlook.com>
2025-05-05 14:15:54 -0400

    start of lab work

---
+++ script.js
@@ -0,0 +1,34 @@
+let csv = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26'
+
+let cell = ''
+let table = []
+let row = []
+
+for (let i = 0; i < csv.length; i++) {
+	let char = csv[i] 
+	//console.log(char)
+
+	if (char !== ',' && char !== '\n') {
+		cell = cell + char
+	} 
+
+	if (char === ',') {
+		row.push(cell)
+		cell = ''
+	} 
+
+	if (char === '\n' || i === csv.length - 1) {
+		row.push(cell)
+		cell = ''
+		table.push(row)
+		row = []
+	}
+}
+
+console.log(table)
+
+
+
+
+
+
